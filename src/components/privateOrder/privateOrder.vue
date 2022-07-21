43d0059110976ca57cec70e4a7f78a16d6342a31
酸奶羊驼 <2925497095@qq.com>
2022-07-21 22:39:39 +0800

    私人订制

--- src/components/privateOrder/privateOrder.vue
+++ src/components/privateOrder/privateOrder.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="privateOrder" ref="privateOrder">
-    <div class="content" >
-      <div class="formBox" >
+    <div class="content">
+      <div class="formBox">
         <h3 class="title">私人定制</h3>
         <el-form
           :model="ruleForm"
@@ -11,31 +11,50 @@
           label-width="100px"
           class="demo-ruleForm"
         >
-          <el-form-item label="密码" prop="pass">
+          <el-form-item label="email" prop="email">
             <el-input
-              placeholder="测试"
-              type="password"
-              v-model="ruleForm.pass"
-              autocomplete="off"
+              placeholder="请输入email地址"
+              type="text"
+              v-model="ruleForm.email"
             ></el-input>
           </el-form-item>
-          <el-form-item label="确认密码" prop="checkPass">
+          <el-form-item label="联系人" prop="contact">
             <el-input
-              type="password"
-              v-model="ruleForm.checkPass"
-              autocomplete="off"
+              placeholder="请输入联系方式"
+              type="text"
+              v-model="ruleForm.contact"
             ></el-input>
           </el-form-item>
-          <el-form-item label="年龄" prop="age">
-            <el-input v-model.number="ruleForm.age"></el-input>
+          <el-form-item label="联系电话" prop="phoneNum">
+            <el-input
+              placeholder="请输入联系电话"
+              type="text"
+              v-model="ruleForm.phoneNum"
+            ></el-input>
           </el-form-item>
-          <el-form-item>
-            <el-button type="primary" @click="submitForm('ruleForm')"
-              >提交</el-button
-            >
-            <el-button @click="resetForm('ruleForm')">重置</el-button>
+          <el-form-item label="联系单位" prop="contactUnit">
+            <el-input
+              placeholder="请输入单位名称"
+              type="text"
+              v-model="ruleForm.contactUnit"
+            ></el-input>
           </el-form-item>
+          <el-form-item label="需求概述" prop="demand" class="demandBox">
+            <el-input
+              class="demand"
+              placeholder="请输入产品名称，CAS号，规格，数量"
+              type="textarea"
+              v-model="ruleForm.demand"
+            ></el-input>
+          </el-form-item>
+          <p class="contactUs">联系方式&nbsp;:&nbsp;labservice@188.com</p>
         </el-form>
+        <el-form-item class="btnBox">
+          <el-button type="primary" @click="submitForm('ruleForm')"
+            >提交</el-button
+          >
+          <el-button @click="resetForm('ruleForm')">重置</el-button>
+        </el-form-item>
       </div>
     </div>
   </div>
@@ -43,57 +62,65 @@
 <script>
 export default {
   data() {
-    var checkAge = (rule, value, callback) => {
+    var checkEmail = (_, value, callback) => {
       if (!value) {
-        return callback(new Error("年龄不能为空"));
+        return callback(new Error("邮箱不能为空"));
       }
-      setTimeout(() => {
-        if (!Number.isInteger(value)) {
-          callback(new Error("请输入数字值"));
-        } else {
-          if (value < 18) {
-            callback(new Error("必须年满18岁"));
-          } else {
-            callback();
-          }
-        }
-      }, 1000);
+      let pattern = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+      if(!pattern.test(value))
+      callback(new Error("邮箱格式不正确"))
+      callback();
     };
-    var validatePass = (rule, value, callback) => {
-      if (value === "") {
-        callback(new Error("请输入密码"));
-      } else {
-        if (this.ruleForm.checkPass !== "") {
-          this.$refs.ruleForm.validateField("checkPass");
-        }
-        callback();
+    var checkContact = (_, value, callback) => {
+      if (!value) {
+        return callback(new Error("联系人不能为空"));
       }
+      callback();
     };
-    var validatePass2 = (rule, value, callback) => {
-      if (value === "") {
-        callback(new Error("请再次输入密码"));
-      } else if (value !== this.ruleForm.pass) {
-        callback(new Error("两次输入密码不一致!"));
-      } else {
-        callback();
+    var checkPhoneNum = (_, value, callback) => {
+      if (!value) {
+        return callback(new Error("联系电话不能为空"));
       }
+      let pattern =
+        /((\d{11})|^((\d{7,8})|(\d{4}|\d{3})-(\d{7,8})|(\d{4}|\d{3})-(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d)|(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d))$)/;
+      if (!pattern.test(value)) {
+        callback(new Error("电话格式不正确"));
+      }
+      callback();
     };
+    var checkContactUnit = (_, value, callback) => {
+      if (!value) {
+        return callback(new Error("联系单位不能为空"));
+      }
+      callback();
+    };
+    var checkDemand = (_, value, callback) => {
+      if (!value) {
+        return callback(new Error("需求概述不能为空"));
+      }
+      callback();
+    };
+
     return {
       ruleForm: {
-        pass: "",
-        checkPass: "",
-        age: "",
+        email: "",
+        contact: "",
+        phoneNum: "",
+        contactUnit: "",
+        demand: "",
       },
       rules: {
-        pass: [{ validator: validatePass, trigger: "blur" }],
-        checkPass: [{ validator: validatePass2, trigger: "blur" }],
-        age: [{ validator: checkAge, trigger: "blur" }],
+        email: [{ validator: checkEmail, trigger: "blur" }],
+        contact: [{ validator: checkContact, trigger: "blur" }],
+        phoneNum: [{ validator: checkPhoneNum, trigger: "blur" }],
+        contactUnit: [{ validator: checkContactUnit, trigger: "blur" }],
+        demand: [{ validator: checkDemand, trigger: "blur" }],
       },
     };
   },
-   mounted() {
+  mounted() {
     document.addEventListener("mousedown", (e) => {
-      let privateOrder = this.$refs.privateOrder; 
+      let privateOrder = this.$refs.privateOrder;
       if (!e.path.includes(privateOrder)) {
         this.$parent.isShow = false;
       }
@@ -113,9 +140,9 @@ export default {
     resetForm(formName) {
       this.$refs[formName].resetFields();
     },
-    closeThis(){
-        this.$parent.isShow = false;
-    }
+    closeThis() {
+      this.$parent.isShow = false;
+    },
   },
 };
 </script>
@@ -137,11 +164,63 @@ export default {
   border-radius: 5px;
 }
 .el-input__inner {
+  width: 367px;
+  height: 41px;
+  border-radius: 5px;
   border: 1px solid #999999;
 }
 .el-input__inner:focus,
 .el-input__inner:hover {
   border: 1px solid var(--color);
+}
+
+.el-form-item .demand {
+  width: 367px;
+  height: 103px;
+}
+
+.el-form-item .el-textarea__inner {
+  width: 367px;
+  height: 103px;
+  border: 1px solid #999999;
+  border-radius: 5px;
+  resize: none;
+}
+.demandBox .el-form-item__content {
+  width: 367px;
+  height: 103px;
+}
+.el-form-item {
+  margin-bottom: 37px;
+}
+.btnBox .el-form-item__content {
+  display: flex;
+  justify-content: center;
+}
+.el-button:nth-child(1) {
+  width: 69px;
+  height: 38px;
+  background: var(--color);
+  border-radius: 5px;
+}
+.el-button:nth-child(2) {
+  width: 69px;
+  height: 38px;
+  color: var(--color);
+  border-radius: 5px;
+  border-color: var(--color);
+}
+.el-button--default:hover {
+  background: #a5d0ff;
+}
+
+/* 后置对钩或叉 */
+
+.el-input__icon{
+ transform: scale(1.5);
+}
+.el-icon-circle-check{
+  color: rgb(74, 189, 150);
 }
 </style>
 <style scoped>
@@ -167,8 +246,10 @@ export default {
 .formBox {
   display: flex;
   flex-direction: column;
-  justify-content: space-around;
+  justify-content: space-between;
   align-items: center;
+  height: calc(812px - 94px);
+  padding: 40px 0 54px 0;
 }
 .title {
   font-size: 36px;
@@ -177,5 +258,12 @@ export default {
   color: #004ea2;
   padding-bottom: 38px;
   border-bottom: 4px solid #004ea2;
+}
+.contactUs {
+  margin: 54px 25px 0 25px;
+  font-size: 16px;
+  font-family: Microsoft YaHei UI;
+  font-weight: 400;
+  color: #666666;
 }
 </style>
