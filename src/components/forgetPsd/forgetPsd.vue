36d85fc9428730822e3e20ee113cc9b05f302d1c
酸奶羊驼 <2925497095@qq.com>
2022-07-22 13:19:00 +0800

    密码登录和忘记密码页

---
+++ src/components/forgetPsd/forgetPsd.vue
@@ -0,0 +1,139 @@
+//createDate:2022-07-22
+<template>
+  <div class="forgetPsd">
+    <div class="content">
+      <h2 class="title" ref="title">找回密码</h2>
+      <div class="inputBox">
+        <input type="text" class="eachInput" :placeholder="inputEmail" />
+      </div>
+      <div class="inputBox verificationBox">
+        <input
+          type="text"
+          class="verification eachInput"
+          :placeholder="inputVerification"
+        />
+        <span class="getCodeBtn" ref="getCodeBtn">获取验证码</span>
+      </div>
+      <div class="inputBox">
+        <input type="text" class="eachInput" placeholder="请输入新密码"/>
+      </div>
+      <div class="inputBox">
+        <input type="text" class="eachInput" placeholder="请确认密码"/>
+      </div>
+      <div class="submit">提交</div>
+      <div class="returnLogIn" @click="returnLogIn()">返回登录</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "forgetPsd",
+  data() {
+    return {
+      inputEmail: "请输入邮箱",
+      inputVerification: "请输入验证码",
+    };
+  },
+  methods: {
+    returnLogIn() {
+      this.$parent.isLogIn = true;
+    },
+  },
+};
+</script>
+<style scoped>
+.content {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  width: 546px;
+  height: 525px;
+  padding-top: 58px;
+  padding-bottom: 48px;
+  margin: 0 auto;
+  border-radius: 5px;
+}
+.title {
+  font-size: 48px;
+  font-family: Microsoft YaHei UI;
+  font-weight: bold;
+  color: var(--color);
+}
+.inputBox {
+  width: 395px;
+  height: 64px;
+  background: #f4f4f4;
+  border-radius: 5px;
+}
+input {
+  font-size: 18px;
+  font-family: Microsoft YaHei UI;
+  font-weight: 400;
+  padding-left: 20px;
+  border: 1px #f4f4f4 solid;
+  transition: 0.3s;
+}
+input:focus{
+    border-color: var(--color);
+}
+input::-webkit-input-placeholder {
+  font-size: 18px;
+  font-family: Microsoft YaHei UI;
+  font-weight: 400;
+  color: #999999;
+}
+.eachInput {
+  width: 375px;
+  height: 64px;
+  background: #f4f4f4;
+  border-radius: 5px;
+}
+.verification {
+  width: calc(375px - 124px);
+  padding-right: 124px;
+}
+.verificationBox {
+  position: relative;
+}
+.getCodeBtn {
+  position: absolute;
+  top: 16px;
+  right: 22px;
+  display: inline-block;
+  height: 33px;
+  line-height: 33px;
+  font-family: Microsoft YaHei UI;
+  font-weight: 400;
+  color: var(--color);
+  border-left: 1px solid #999999;
+  padding-left: 12px;
+  cursor: pointer;
+}
+.submit {
+  width: 394px;
+  height: 61px;
+  background: var(--color);
+  border-radius: 5px;
+  font-size: 24px;
+  font-family: Microsoft YaHei UI;
+  font-weight: 400;
+  color: #ffffff;
+  line-height: 61px;
+  text-align: center;
+  cursor: pointer;
+  transition: all 0.3s;
+}
+.submit:hover {
+  box-shadow: 0px 4px 16px -6px var(--color);
+  --webkit-box-shadow: 0px 4px 16px -6px var(--color);
+  --moz-box-shadow: 0px 4px 16px -6px var(--color);
+}
+.returnLogIn {
+  font-size: 18px;
+  font-family: Microsoft YaHei UI;
+  font-weight: 400;
+  color: var(--color);
+  cursor: pointer;
+}
+</style>
--- src/components/logIn/logIn.vue
+++ src/components/logIn/logIn.vue
@@ -1,39 +1,87 @@
 <template>
   <div class="logIn">
     <div class="content" ref="logInContent">
-      <h2 class="title">用户登录</h2>
-
-      <div class="inputBox">
-        <input type="text" class="email" :placeholder="inputEmail" />
+      <div class="logInBox" v-show="isLogIn">
+        <h2 class="title" ref="title">用户登录</h2>
+        <div class="inputBox" data-after="">
+          <input
+            type="text"
+            class="email"
+            :placeholder="inputEmail"
+            v-model="logInForm.email"
+            @blur="checkEmail($event)"
+          />
+        </div>
+        <div class="inputBox verificationBox" data-after="">
+          <input
+            ref="psd_verification"
+            type="text"
+            class="verification"
+            :placeholder="inputVerification"
+            v-model="logInForm.psd"
+            @blur="checkVerification($event)"
+          />
+          <span class="getCodeBtn" ref="getCodeBtn">获取验证码</span>
+        </div>
+        <div class="psd_register">
+          <span class="psdLogIn" @click="changeLogIn()" ref="logInWay"
+            >密码登录</span
+          >
+          <span class="register" @click="toRegister()">立即注册 </span>
+        </div>
+        <div class="logInBtn">登录</div>
+        <div class="forgotPsd" @click="toForgetPsd()">忘记密码?</div>
       </div>
-      <div class="inputBox">
-        <input
-          type="text"
-          class="Verification"
-          :placeholder="inputVerification"
-        />
-        <span class="getCodeBtn">获取验证码</span>
-      </div>
-      <div class="psd_register">
-        <span class="psdLogIn">密码登录</span>
-        <span class="register">立即注册 </span>
-      </div>
-      <div class="logInBtn">登录</div>
-      <div class="forgotPsd">忘记密码?</div>
+      <forget-psd v-show="!isLogIn" />
     </div>
   </div>
 </template>
 
 <script>
+import forgetPsd from "../forgetPsd/forgetPsd.vue";
 export default {
   name: "logIn",
+  components: {
+    forgetPsd,
+  },
   data() {
     return {
+      //登录表单
+      logInForm: {
+        email: "",
+        psd: "",
+      },
       inputEmail: "请输入邮箱",
       inputVerification: "请输入验证码",
+      pageCode: 1, // 1为验证码登录,2为密码登录,3为忘记密码页,默认为 1
+      isLogIn: true, // t显示登录页 f显示忘记密码页
     };
   },
+  watch: {
+    pageCode: {
+      handler(newCode) {
+        switch (newCode) {
+          case 1:
+            this.$refs.logInWay.innerHTML = "密码登录";
+            this.$refs.getCodeBtn.style.display = "inline-block";
+            this.inputVerification = "请输入验证码";
+            this.$refs.psd_verification.type = "text";
+            break;
+          case 2:
+            this.$refs.logInWay.innerHTML = "验证码登录";
+            this.$refs.getCodeBtn.style.display = "none";
+            this.inputVerification = "请输入密码";
+            this.$refs.psd_verification.type = "password";
 
+            break;
+          case 3:
+            break;
+          default:
+            break;
+        }
+      },
+    },
+  },
   mounted() {
     // 监听鼠标点击  点击位置为登录之外,则关闭登录页
     document.addEventListener("mousedown", (e) => {
@@ -43,7 +91,54 @@ export default {
       } else this.$parent.isShowLogIn = true;
     });
   },
-  methods: {},
+  methods: {
+    toRegister() {
+      this.$router.push("/register");
+      this.$parent.isShowLogIn = false;
+    },
+    // 切换登录方式
+    changeLogIn() {
+      switch (this.$refs.logInWay.innerText) {
+        case "密码登录":
+          this.pageCode = 2;
+          break;
+        case "验证码登录":
+          this.pageCode = 1;
+          break;
+        default:
+          break;
+      }
+    },
+    // 忘记密码
+    toForgetPsd() {
+      this.isLogIn = false;
+    },
+    // 验证邮箱合法性
+    checkEmail(e) {
+      let a = e.path[1]; // 获取带有after伪元素的父盒子
+      if (this.logInForm.email == "") {
+        a.setAttribute("data-after", "邮箱不能为空");
+        return;
+      }
+      let pattern = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+      if (!pattern.test(this.logInForm.email)) {
+        a.setAttribute("data-after", "邮箱格式不正确");
+      } else {
+        a.setAttribute("data-after", "");
+      }
+    },
+
+    // 验证验证码
+    checkVerification(e) {
+      let a = e.path[1]; // 获取带有after伪元素的父盒子
+      if (this.logInForm.email == "") {
+        a.setAttribute("data-after", "验证码不能为空");
+      } else {
+        a.setAttribute("data-after", "");
+      }
+      return;
+    },
+  },
 };
 </script>
 
@@ -56,8 +151,20 @@ export default {
   left: 0;
   right: 0;
   top: 0;
+  z-index: 999;
 }
 .content {
+  width: 546px;
+  height: 631px;
+  margin: 0 auto;
+  background: #ffffff;
+  border-radius: 5px;
+  box-shadow: 0px 1px 24px -1px #919191;
+  --webkit-box-shadow: 0px 1px 24px -1px #919191;
+  --moz-box-shadow: 0px 1px 24px -1px #919191;
+  margin-top: 10vh;
+}
+.logInBox {
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -67,12 +174,7 @@ export default {
   padding-top: 58px;
   padding-bottom: 148px;
   margin: 0 auto;
-  background: #ffffff;
   border-radius: 5px;
-  box-shadow: 0px 1px 24px -1px #919191;
-  --webkit-box-shadow: 0px 1px 24px -1px #919191;
-  --moz-box-shadow: 0px 1px 24px -1px #919191;
-  margin-top: 10vh;
 }
 .title {
   font-size: 48px;
@@ -81,39 +183,59 @@ export default {
   color: var(--color);
 }
 .inputBox {
+  position: relative;
   width: 395px;
   height: 64px;
   background: #f4f4f4;
   border-radius: 5px;
 }
+.inputBox:after {
+  content: attr(data-after);
+  position: absolute;
+  bottom: -18px;
+  left: 20px;
+  font-size: 12px;
+  color: rgb(191, 23, 23);
+  z-index: 999;
+}
+/* .inputBox::before{
+  content: url("../../assets/hook.png");
+  position: absolute;
+  right: -90px;
+  top: -30px;
+  transform: scale(0.3);
+} */
 input {
   font-size: 18px;
   font-family: Microsoft YaHei UI;
   font-weight: 400;
   padding-left: 20px;
+  width: 375px;
+  height: 64px;
+  background: #f4f4f4;
+  border: 1px #f4f4f4 solid;
+  border-radius: 5px;
+  transition: 0.3s;
 }
-
+input:focus {
+  border-color: var(--color);
+}
 input::-webkit-input-placeholder {
   font-size: 18px;
   font-family: Microsoft YaHei UI;
   font-weight: 400;
   color: #999999;
 }
-.email {
-  width: 375px;
-  height: 64px;
-  background: #f4f4f4;
-  border-radius: 5px;
-}
-.Verification {
-  width: 250px;
-  height: 64px;
-  background: #f4f4f4;
-  border-radius: 5px;
-}
 
+.verificationBox {
+  position: relative;
+}
 .getCodeBtn {
+  position: absolute;
+  top: 16px;
+  right: 22px;
   height: 33px;
+  line-height: 33px;
   font-size: 18px;
   font-family: Microsoft YaHei UI;
   font-weight: 400;
