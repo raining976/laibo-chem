b436a139fbfb333356fda0e71d87cee3d0ced22e
yogurt_alpaca <29254970952qq.com>
2022-09-04 09:30:22 +0800

    将六位区号转化为文字的全局函数

---
+++ src/js/handlerAddress.js
@@ -0,0 +1,15 @@
+// 处理地址 将区号转化成字符串
+import {  CodeToText } from "element-china-area-data";
+export default function handleAddress(array) {
+    array.forEach((item) => {
+        let codeString = String(item.sx);
+        let addressCode = [];
+        var address = "";
+        addressCode.push(codeString.slice(0, 2) + "0000");
+        addressCode.push(codeString.slice(0, 4) + "00");
+        addressCode.push(codeString);
+        address = CodeToText[addressCode[0]] + CodeToText[addressCode[1]] + CodeToText[addressCode[2]] + item.dz
+        item.address = address
+    });
+    return array;
+}
