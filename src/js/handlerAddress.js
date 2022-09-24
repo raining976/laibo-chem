12cdf66dc7e7f1b3987c1f6bc4aaf17151185b14
yogurt_alpaca <29254970952qq.com>
2022-09-24 18:47:56 +0800

    地址转化行政区号为空时的判断

--- src/js/handlerAddress.js
+++ src/js/handlerAddress.js
@@ -1,15 +1,20 @@
 // 处理地址 将区号转化成字符串
-import {  CodeToText } from "element-china-area-data";
+import { CodeToText } from "element-china-area-data";
 export default function handleAddress(array) {
     array.forEach((item) => {
-        let codeString = String(item.sx);
-        let addressCode = [];
-        var address = "";
-        addressCode.push(codeString.slice(0, 2) + "0000");
-        addressCode.push(codeString.slice(0, 4) + "00");
-        addressCode.push(codeString);
-        address = CodeToText[addressCode[0]] + CodeToText[addressCode[1]] + CodeToText[addressCode[2]] + item.dz
-        item.address = address
+        if (item.sx) {
+            let codeString = String(item.sx);
+            let addressCode = [];
+            var address = "";
+            addressCode.push(codeString.slice(0, 2) + "0000");
+            addressCode.push(codeString.slice(0, 4) + "00");
+            addressCode.push(codeString);
+            address = CodeToText[addressCode[0]] + CodeToText[addressCode[1]] + CodeToText[addressCode[2]] + item.dz
+            item.address = address
+        } else {
+            item.address = item.dz
+        }
+
     });
     return array;
 }
