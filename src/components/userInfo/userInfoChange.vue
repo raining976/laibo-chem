f6c0302dea37e9171149129578c4d8934ae2e0ef
yogurt_alpaca <29254970952qq.com>
2022-09-30 21:38:26 +0800

    解决个人信息不能修改的问题

--- src/components/userInfo/userInfoChange.vue
+++ src/components/userInfo/userInfoChange.vue
@@ -54,11 +54,11 @@ export default {
           new Error(this.$t("form.enter") + this.$t("base.phone"))
         );
       }
-      var pattern =
-        /^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/;
+      var pattern = /^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/
       if (!pattern.test(value)) {
         return callback(new Error("手机号格式错误"));
       }
+      return callback();
     };
     return {
       initName: "", // 原来的名字
