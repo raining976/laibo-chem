2b59bc9b4be5dc8118303868ea6da4b43b58cff4
yogurt-alpaca <2925497095@qq.comgit config --global user.email 2925497095@qq.com>
2022-12-16 14:19:22 +0800

    备案号和链接

--- src/components/main/copyRight.vue
+++ src/components/main/copyRight.vue
@@ -3,13 +3,13 @@
     <div class="content">
       <div class="blue">
         Copyright © 2022 Laibo Chem Inc. - All rights reserved.
-        沪ICP备xxxxxxxxxxx号
+        鲁ICP备2022020172号-1
       </div>
       <div class="red">
         本网站销售的所有产品仅用于工业应用或者科学研究等非医疗目的，不可用于人类或动物的临床诊断或治疗，非药用，非食用。
       </div>
       <div class="blue bottom">
-        <span>沪公网安备xxxxxxxxxxxxxxxxxxx号</span>
+        <span><a href="https://beian.miit.gov.cn/" target="blank">鲁ICP备2022020172号-1</a></span>
         <span>危险品化学经营许可证（带存储）</span>
       </div>
     </div>
@@ -46,6 +46,9 @@ export default {
   font-family: Microsoft YaHei UI;
   font-weight: 400;
   color: #014a97;
+}
+.blue a{
+    color: #014a97;   
 }
 .red {
   font-size: 0.73vw;
